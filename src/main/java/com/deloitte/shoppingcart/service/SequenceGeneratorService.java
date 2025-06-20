d4bf35977f1bfccd4a5977cca49dfc6be70c76f8
Udayasree, Udayasree <uudayasree+deloitte@deloitte.com>
2025-06-20 12:48:57 +0530

    removed space

--- src/main/java/com/deloitte/shoppingcart/service/SequenceGeneratorService.java
+++ src/main/java/com/deloitte/shoppingcart/service/SequenceGeneratorService.java
@@ -27,6 +27,7 @@ public class SequenceGeneratorService {
 
 		DatabaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
 				new Update().inc("seq", 1), options().returnNew(true).upsert(true), DatabaseSequence.class);
+
 		return !Objects.isNull(counter) ? counter.getSeq() : 1;
 	}
 
