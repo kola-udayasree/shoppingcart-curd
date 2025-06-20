0e7264d4acd9fc712a4832d473b9148f015a513b
Udayasree, Udayasree <uudayasree+deloitte@deloitte.com>
2025-06-20 20:53:04 +0530

    removed space

--- src/main/java/com/deloitte/shoppingcart/controller/ProductController.java
+++ src/main/java/com/deloitte/shoppingcart/controller/ProductController.java
@@ -31,7 +31,6 @@ public class ProductController {
 	 */
 	@GetMapping("/products")
 	public List<Product> getProducts() {
-
 		logger.debug("---Getting all products---");
 		List<Product> productList = this.productRepository.findAll();
 		logger.debug("Product List: "+productList);
