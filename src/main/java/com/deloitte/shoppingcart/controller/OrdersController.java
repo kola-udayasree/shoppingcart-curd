34d39e21ba796f2a39f75696ada8db08abcbc381
Udayasree, Udayasree <uudayasree+deloitte@deloitte.com>
2025-06-24 14:50:08 +0530

    log statements

--- src/main/java/com/deloitte/shoppingcart/controller/OrdersController.java
+++ src/main/java/com/deloitte/shoppingcart/controller/OrdersController.java
@@ -62,8 +62,7 @@ public class OrdersController {
 	@GetMapping("/orders/{id}")
 	public ResponseEntity<Order> getOrderById(@PathVariable(value = "id") Long orderId)
 			throws ResourceNotFoundException {
-
-		logger.debug(" --- Getting Order details based on given OrderId --- ");
+		logger.debug(" --- Getting Order Details based on given OrderId --- ");
 		Order order = this.orderRepository.findById(orderId)
 				.orElseThrow(() -> new ResourceNotFoundException(" Order not found for :: " + orderId));
 		return new ResponseEntity<Order>(order, HttpStatus.OK);
